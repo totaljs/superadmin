5a4d6bf89e80685537da2c1fdc6ab77b75ebb629
Peter Širka <petersirka@gmail.com>
2016-07-20 21:30:20 +0200

    Fixed useless message with kill.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-kill -9 $(lsof -i :9999 | grep "total" | awk {'print $2'})
-node --nouse-idle-notification --expose-gc --max_inlined_source_size=1200 debug.js 9999 > superadmin.log &
+kill -9 $(lsof -i :9999 | grep "total" | awk {'print $2'}) > /dev/null
+node --nouse-idle-notification --expose-gc --max_inlined_source_size=1200 release.js 9999 > superadmin.log &
