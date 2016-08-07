3bd6740c45bea064fd40ff506f97df9a326422ed
Athul B Raj <mail@athulraj.com>
2016-08-07 18:07:53 +0530

    Update install.sh
    Take user consent in one line.
    Check root permission
    remove use of sudo

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-sudo apt-get install python-software-properties
-sudo apt-get install software-properties-common
+echo "SuperAdmin Installtion"
+# Root check
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+fi
+echo "This setup requires the installation of the nginx, nodejs and graphicsmagick packages using apt-get"
+echo "Do you wish to permit this ? (y/n)"
+read userConsent
+if [ "$userConsent" == "y" ]; then
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+apt-get install python-software-properties
+apt-get install software-properties-common
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get update
-sudo apt-get install nginx
-sudo apt-get install -y nodejs
-sudo apt-get install graphicsmagick
+apt-get update
+apt-get install -y nginx
+apt-get install -y nodejs
+apt-get install -y graphicsmagick
 curl https://get.acme.sh | sh
 mkdir /www/
 mkdir /www/logs/
@@ -16,3 +26,6 @@ mkdir /www/www/
 mkdir /www/node_modules/
 cd /www/
 npm install total.js@beta
+else
+echo "Sorry, this installation cannot continue."
+fi
