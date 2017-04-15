6716a3a4bddb912f77f3ae20710f019fb1a5b432
Peter Širka <petersirka@gmail.com>
2017-04-15 17:02:19 +0200

    Update `jComponent` version + fix a problem with j-Form.

--- public/js/ui.js
+++ public/js/ui.js
@@ -878,7 +878,7 @@ COMPONENT('form', function() {
 		var el = $('#' + self._id);
 		el.find('.ui-form').get(0).appendChild(self.element.get(0));
 		self.element.removeClass('hidden');
-		self.element = el;
+		self.replace(el);
 
 		self.element.on('scroll', function() {
 			EXEC('$calendar.hide');
